1838ea6da36a6bf840f8a7517ce872a127aa9fb8
Ednei Freitas <edneifneto@gmail.com>
2018-11-22 17:00:08 -0200

    commit comando linux

--- Tutorial/tutorial_android_atualizado.java
+++ Tutorial/tutorial_android_atualizado.java
@@ -4,6 +4,43 @@
 //====================================================================
 
 //====================================================================
+//COMANDO LINUX: 
+//====================================================================
+package broadcast.com.br.lausherprojectsv2.utils;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TerminalUtil {
+
+   public static String TAG = "TerminalLog";
+   public static String Executar(String cmd) {
+      try {
+
+         Process process = Runtime.getRuntime().exec(cmd);
+         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+         int read;
+         char[] buffer = new char[4096];
+         StringBuffer output = new StringBuffer();
+         while ((read = reader.read(buffer)) > 0) {
+            output.append(buffer, 0, read);
+         }
+         reader.close();
+         process.waitFor();
+         return output.toString();
+      } catch (IOException e) {
+         throw new RuntimeException(e);
+      } catch (InterruptedException e) {
+         throw new RuntimeException(e);
+      }
+   }
+}
+//====================================================================
+//FIM
+//====================================================================
+
+//====================================================================
 //MAC ADDRESS: 
 //====================================================================
 public static String getMacAddrWifi() {
