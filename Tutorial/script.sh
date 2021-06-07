2bda9652fad2f9b32b33b9d8c0d16523298fd24d
Ednei Freitas <edneifneto@gmail.com>
2021-06-07 20:45:02 -0300

    Build automatic

--- Tutorial/script.sh
+++ Tutorial/script.sh
@@ -32,3 +32,70 @@ p)
     echo "nao existe "
     ;;
 esac
+
+
+
+#########################################################
+# BUILd
+#########################################################
+#!/bin/bash
+
+
+##################################################################################
+#   PRE REQUISITOS                                                               #
+##################################################################################
+# Fonts
+# install apt-get install figlet
+# Variável de Ambiente - JAVA_HOME
+
+set -e
+
+clear
+
+sh utils/titles/title_build.sh
+
+# shellcheck disable=SC2116
+cr=$(echo $'\n> ')
+cr=${cr%.}
+
+# shellcheck disable=SC2162
+read -p "Deseja criar Aplicativo  [S/N]: $cr" CONTINUE
+
+# shellcheck disable=SC1009
+if [ "$CONTINUE" = "s" ]
+then
+    cd ..
+
+    printf "\n\nCleaning ...\n"
+    ./gradlew clean
+
+    printf "\n\nCleaning build cache ...\n"
+    ./gradlew cleanBuildCache
+
+    printf "\n\nCreate Apk...\n"
+    ./gradlew assembleDebug
+
+    printf "\n\nCopying...\n"
+
+    RELEASE_DIR="app/src/release"
+
+    if [ -d "$RELEASE_DIR" ]
+    then
+      printf "\n\nDeleting path release...\n"
+      rm -Rf "$RELEASE_DIR"
+    fi
+
+    mkdir -m777 "app/src/release" && cp -Rf "app/build/outputs/apk/debug/app-debug.apk" "app/src/release"
+
+    printf "\n\nCreate apk is success\n"
+
+else
+  printf "\n\nOPERCAO CANCELADA"
+fi
+
+
+
+
+
+
+
